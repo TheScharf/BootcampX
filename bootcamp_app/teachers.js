fa70c2c1825784338686a7982cc089a4e04377ac
Alexandra Scharfenberger <ascharfe55@gmail.com>
2022-03-10 17:41:24 -0700

    create parapeterized queries

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -8,16 +8,20 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
+const queryString = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON students.id = student_id
 JOIN cohorts ON cohorts.id = cohort_id
 WHERE teachers.id = assistance_requests.teacher_id
-AND cohorts.name LIKE '${process.argv[2] || 'JUL02'}'
+AND cohorts.name LIKE $1
 ORDER BY teacher;
-`)
+`
+const cohortName = process.argv[2];
+const values = [`%${cohortName}%`];
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`);
